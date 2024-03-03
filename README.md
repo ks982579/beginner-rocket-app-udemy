d3e3472ef05cce4120e1fd6976e1b8be0a4cd632
Kevin Sullivan <kevinsullivan@ksulldev.space>
2024-03-03 12:35:42 +0000

    adding a catcher for 404

--- README.md
+++ README.md
@@ -8,3 +8,27 @@ How to include dependencies? I like to use `cargo add` as such
 cargo add rocket --features=json
 cargo add serde_json
 ```
+
+HTTP requests should be as follows:
++ GET
++ POST
++ PUT
++ DELETE
+
+In the POST request, you notice we also inclued `format = "json"`.
+This is to tell rust to include that format in the HTTP header. 
+
+Routing is also nice in that, we pass in functions.
+
+```bash
+curl 127.0.0.1:8000/rustaceans -X POST -H 'Content-type: application/json'
+curl 127.0.0.1:8000/rustaceans/42 -X DELETE -I
+```
+
+Seems like the workflow is create a function endpoint with proper macros.
+Then, you _register_ it in the main function.
+For the actual routes, we _mount_ them using the `routes!` macro.
+We then register our errors using the `catchers!` macro.
+
+HTTP Standard of **Basic Access Authentication** (wiki). 
+In the HTTP header, you pass in something like `Authorization: Basic <credentials>`.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,66 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::json::{json, Value};
+use rocket::{
+    response::status,
+    serde::json::{json, Value},
+};
 
 #[get("/")]
 fn hello() -> Value {
     json!("Hello, world!")
 }
 
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    let FAKE_DATA: &[Value] = &[
+        json!({ "id": 1, "name": "John Doe" }),
+        json!({"id": 2, "name": "Jane Doe"}),
+    ];
+
+    json!(FAKE_DATA)
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({ "id": id, "name": "Kevin", "email": "kevin@crazy.com"})
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!({"id": 3, "name": "Lisa", "email": "lisa@crazy.com"})
+}
+
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({ "id": id, "name": "John Doe", "email": "john@crazy.com" })
+}
+
+#[delete("/rustaceans/<id>")]
+fn delete_rustacean(id: i32) -> status::NoContent {
+    status::NoContent
+}
+
+// Error Patterns
+#[catch(404)]
+fn not_found() -> Value {
+    json!("Not Found!")
+}
+
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build()
+        .mount(
+            "/",
+            routes![
+                get_rustaceans,
+                view_rustacean,
+                create_rustacean,
+                update_rustacean,
+                delete_rustacean,
+            ],
+        )
+        .register("/", catchers![not_found])
+        .launch()
+        .await;
 }
