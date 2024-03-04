9b9cc74271470ca63fb981fcc3f38eef5087d15a
Kevin Sullivan <kevinsullivan@ksulldev.space>
2024-03-04 06:37:35 +0000

    Endpoint updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Beginner APIs in Rust
 
+[Moergo Glove](https://www.moergo.com/)
+
 This is just following a Udemy course to practice building web applications in Rust. We will use Rocket for now as it should have a good DX. 
 
 How to include dependencies? I like to use `cargo add` as such
@@ -32,3 +34,34 @@ We then register our errors using the `catchers!` macro.
 
 HTTP Standard of **Basic Access Authentication** (wiki). 
 In the HTTP header, you pass in something like `Authorization: Basic <credentials>`.
+There are other types of authorization like "bearer", but we do not look at it ATM.
+Wiki with the example, you use a colon to separate the username and password an then 
+base64 encode it. 
+Base64 can be decoded.
+The encoding isn't for security but for transporting over the internet. 
+You want HTTPS enabled, and you don't want your basic credentials logged anywhere people can find it. 
+
+In Rust, the `?` tells Rust that if the expression returns a `None` value, stop and return `None` there.
+Else, if it returns a `Some` value, continue through the function. 
+
+Also, must deviate slightly from the Udemy course here because for `base64::decode` I am getting
+a deprecated warning and instructed to use `Engine::decode`. 
+Reading through the [base64 docs](https://docs.rs/base64/latest/base64/), you can import the
+prelude and then use `BASE64_STANDARD.decode()?`.
+Else, you have to instantiate your own engine with configuration.
+
+What is Base64? Check out [base64encoder.io](https://www.base64encoder.io/learn/). 
+It's purpose is to encode binary or non-ASCII text data to printable ASCII format so
+it can be safely transmitted over _any_ communication channel. 
+
+What about Request Guards? Sounds like Middleware.
+There are apparently 2 versions of [rocket outcome](https://docs.rs.rocket/latest/rocket/outcome/)
+One being `rocket:outcome::Outcome::{Success, Error, Forward}` which is the OG.
+The second comes from `rocket::request::Outcome` which is _type alias_ for the 
+actual Outcome, and only requires Success and Forward. 
+
+I'm not 100% sure, but it seems like when you implement the `FromRequest` trait,
+then the struct becomes a trait guard. 
+Maybe in the macros, they try to pull the value from the HTTP request, 
+hense the name...
+Very interesting implementation. 
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,81 @@
 #[macro_use]
 extern crate rocket;
 
+use base64::prelude::*;
+
 use rocket::{
-    response::status,
+    http::Status,
+    request::{FromRequest, Outcome, Request},
+    response::status::{self, Unauthorized},
     serde::json::{json, Value},
 };
 
+// Authentication (should be own module)
+pub struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+// Factory Methods (common in Rust)
+impl BasicAuth {
+    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
+        if split[0] != "Basic" {
+            return None;
+        }
+        Self::from_base64_encoded(split[1])
+    }
+
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = BASE64_STANDARD.decode(base64_string).ok()?;
+        let decoded_str = String::from_utf8(decoded).ok()?;
+        let split = decoded_str.split(":").collect::<Vec<_>>();
+
+        // If exactly username and password were received
+        if split.len() != 2 {
+            return None;
+        }
+        let (username, password) = (split[0].to_string(), split[1].to_string());
+        Some(BasicAuth {
+            username: username,
+            password: password,
+        })
+    }
+}
+
+// Creating a Request Guard
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for BasicAuth {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth_header = request.headers().get_one("Authorization");
+
+        if let Some(auth_header) = auth_header {
+            if let Some(auth) = Self::from_authorization_header(auth_header) {
+                return Outcome::Success(auth);
+            }
+        }
+        Outcome::Error((Status::Unauthorized, ()))
+    }
+}
+
 #[get("/")]
-fn hello() -> Value {
+fn _hello() -> Value {
     json!("Hello, world!")
 }
 
 #[get("/rustaceans")]
-fn get_rustaceans() -> Value {
-    let FAKE_DATA: &[Value] = &[
+fn get_rustaceans(auth: BasicAuth) -> Value {
+    let fake_data: &[Value] = &[
         json!({ "id": 1, "name": "John Doe" }),
         json!({"id": 2, "name": "Jane Doe"}),
     ];
 
-    json!(FAKE_DATA)
+    json!(fake_data)
 }
 
 #[get("/rustaceans/<id>")]
@@ -42,6 +99,10 @@ fn delete_rustacean(id: i32) -> status::NoContent {
 }
 
 // Error Patterns
+#[catch(401)]
+fn unauthorized() -> Value {
+    json!("Unauthorized")
+}
 #[catch(404)]
 fn not_found() -> Value {
     json!("Not Found!")
@@ -60,7 +121,7 @@ async fn main() {
                 delete_rustacean,
             ],
         )
-        .register("/", catchers![not_found])
+        .register("/", catchers![not_found, unauthorized])
         .launch()
         .await;
 }
